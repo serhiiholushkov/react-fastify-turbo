43ed97f363a53382d6e4568e3a0d9c10d2e2cc06
Serhii Holushkov <serhii.holushkov@gmail.com>
2026-05-18 02:06:39 -0400

    Setting up the repo: config packages

---
+++ packages/eslint-config/node.js
@@ -0,0 +1,16 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { config as baseConfig } from './base.js';
+
+/** @type {import("typescript-eslint").Config} */
+export default tseslint.config(...baseConfig, {
+  languageOptions: {
+    globals: {
+      ...globals.node,
+    },
+  },
+  rules: {
+    '@typescript-eslint/no-floating-promises': 'error', // critical for async Fastify handlers
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+  },
+});
