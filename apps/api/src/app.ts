fa9a4c8103950be87bf3336fa62b77c1caab50c9
Serhii Holushkov <serhii.holushkov@gmail.com>
2026-06-24 11:51:49 -0400

    fixing cors issue

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,10 +1,14 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import dbPlugin from "./plugins/db.plugin.js";
 import tasksPlugin from "./features/tasks/tasks.plugin.js";
 
 export async function buildApp() {
   const server = fastify();
 
+  await server.register(cors, {
+    origin: process.env.CORS_ORIGIN ?? "http://localhost:3000",
+  });
   await server.register(dbPlugin);
   await server.register(tasksPlugin);
 
