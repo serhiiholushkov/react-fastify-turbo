975c7894a8468e1182b9cafd488576de572239a9
Serhii Holushkov <serhii.holushkov@gmail.com>
2026-07-09 15:13:14 -0400

    split entities in protos

--- packages/api-rpc/src/index.ts
+++ packages/api-rpc/src/index.ts
@@ -1,1 +1,5 @@
-export * from "./gen/workspace/v1/task_service_pb.js";
+export * from "./gen/workspace/v1/assignee_pb.js";
+export * from "./gen/workspace/v1/priority_pb.js";
+export * from "./gen/workspace/v1/project_pb.js";
+export * from "./gen/workspace/v1/status_pb.js";
+export * from "./gen/workspace/v1/task_pb.js";
